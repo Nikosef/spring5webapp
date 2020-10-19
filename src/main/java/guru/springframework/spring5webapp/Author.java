28d6ae90cf56cce8cdb51a90de846d445c5325b4
Nikosef <n-efthymiou@outlook.com>
2020-10-19 23:35:25 +0300

    first commit

---
+++ src/main/java/guru/springframework/spring5webapp/Author.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5webapp;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Author {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+}
